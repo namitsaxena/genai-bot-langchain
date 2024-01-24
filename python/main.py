f5a2be95b558e92937a1352d125d809109b3170a
namit <namit@namitsaxena.com>
2024-01-23 19:14:58 -0600

    Vertex API - 2nd Iteration version with only text-bison enabled- but temperature, etc other params added

--- python/main.py
+++ python/main.py
@@ -17,6 +17,7 @@ REGION = "us-central1"  # @param {type:"string"}
 # Initialize Vertex AI SDK
 vertexai.init(project=PROJECT_ID, location=REGION)
 
+# Import only what is strictly needed
 from vertexai.language_models import (
     TextGenerationModel,
     # TextEmbeddingModel,
@@ -26,16 +27,53 @@ from vertexai.language_models import (
     # CodeChatModel,
 )
 
+
 # Uses only TextGenerationModel (From vertexai)
-def text_generation(prompt="What is a large language model?", model="text-bison@001"):
+def text_generation(
+        prompt="What is a large language model?"
+        , temperature=0.0  # 0.0 - 1.0
+        , max_output_tokens=128  # 1-1024
+        , top_p=0.95  # 0.0 - 1.0
+        , top_k=40  # 0.0 - 40
+        , model="text-bison@001"
+):
     print(f'Querying: model: {model}, Prompt: {prompt}')
     generation_model = TextGenerationModel.from_pretrained(model)
-    response = generation_model.predict(prompt=prompt)
+    response = generation_model.predict(
+        prompt=prompt
+        , temperature=temperature
+        , max_output_tokens=max_output_tokens
+        , top_p=top_p
+        , top_k=top_k
+    )
     print(response.text)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # print_hi('PyCharm')
-    text_generation("What is an embedding in an LLM", "text-bison@001")
+    my_industry = "tech"
+    # prompt example example
+    prompt = f"""Create a numbered list of 10 items. Each item in the list should
+        be a trend in the {my_industry} industry.
+
+        Each trend should be less than 5 words."""
+
+    # temperature example prompt
+    # prompt = "Complete the sentence: As I prepared the picture frame, I reached into my toolkit to fetch my:"
+
+    # top_p example
+    prompt = "Create a marketing campaign for jackets that involves blue elephants and avocados."
+
+    # top_k example
+    prompt = "Write a 2-day itinerary for France."
+
+    text_generation(
+          prompt=prompt
+        , temperature=0
+        , max_output_tokens=1024
+        , top_p=0.95
+        , top_k=40
+    )
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
